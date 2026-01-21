b82fae803241ebd035c6659332fe11478e857614
JP Del Mundo <JP@CZ>
2026-01-22 07:47:36 +0800

    Updated: Backup comments, deleted unused Dockerfile

--- db/scripts/backup.sh
+++ db/scripts/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#run in project root
+#run in project root (in wsl)
 TS=$(date +%Y%m%d_%H%M%S)
 docker exec -t pgdb pg_dump -U postgres jpdm > ./db/backup/jpdm_${TS}.sql
 docker exec -t pgdb pg_dump -U postgres --schema-only jpdm > ./db/backup/jpdm_schema_${TS}.sql
