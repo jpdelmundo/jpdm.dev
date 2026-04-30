d369ec78f5812b93dbd2923acf4932f9dfb4b520
JP Del Mundo <JP@CZ>
2026-04-30 13:00:36 +0800

    Added: Main menu responsive layout

--- frontend/src/pages/public/Layout.tsx
+++ frontend/src/pages/public/Layout.tsx
@@ -2,42 +2,110 @@ import { UserPanel } from '@/components/UserPanel';
 import { Outlet, Link as RLink } from 'react-router-dom';
 
 import { GradientBg } from '@/components/GradientBg.tsx';
-import Box from '@mui/material/Box';
+import { theme } from '@/themes/theme.ts';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import Container from '@mui/material/Container';
 import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import Stack from '@mui/material/Stack';
 import type { SxProps } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useState } from 'react';
+
+type MenuItem = { label: string; to: string; visible: boolean };
 
 const linkSx: SxProps = {
     color: '#000000',
     textDecoration: 'none',
 };
 
+const menuItems: MenuItem[] = [
+    { label: 'Home', to: '/', visible: true },
+    { label: 'Projects', to: '/projects', visible: true },
+    { label: 'Homelab', to: '/homelab', visible: true },
+    { label: 'About', to: '/about', visible: true },
+    { label: 'Hire Me', to: '/services', visible: false },
+];
+
 export const Layout = () => {
+    const isSmDown = useMediaQuery(theme.breakpoints.down('sm'));
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const onMenuClick = () => setDrawerOpen(prev => !prev);
+
     return (
         <>
             <GradientBg />
+            <Drawer
+                open={drawerOpen && isSmDown}
+                onClose={() => setDrawerOpen(false)}
+                sx={{
+                    '& .MuiDrawer-paper': {
+                        width: '240px'
+                    }
+                }}
+            >
+                <List>
+                    <Typography
+                        sx={{
+                            fontFamily: 'Anton SC, sans-serif',
+                            fontSize: '25px',
+                            color: 'black',
+                            textDecoration: 'none',
+                            padding: '8px 16px',
+                        }}
+                        tabIndex={-1}
+                    >JPDM</Typography>
+                    {menuItems.filter((item) => item.visible).map((item, index, arr) => (
+                        <ListItem disablePadding>
+                            <ListItemButton component={RLink} to={item.to} sx={linkSx} onClick={() => setDrawerOpen(false)} sx={{ px: 4 }}>
+                                <ListItemText>{item.label}</ListItemText>
+                            </ListItemButton>
+                        </ListItem>
+
+                        // <>
+                        //     <Link component={RLink} to={item.to} sx={linkSx}>{item.label}</Link>
+                        //     {index < arr.length - 1 && <Divider orientation="vertical" flexItem />}
+                        // </>
+                    ))}
+                </List>
+            </Drawer>
             <Container component="header" className="header-container" sx={{ height: '60px', zIndex: 1000 }}>
                 <Stack direction="row" alignItems="center" maxWidth="md" sx={{ margin: '0 auto', gap: 2 }}>
-                    <Box sx={{ mr: 'auto' }}>
+                    <Stack direction="row" gap={2} alignItems={'center'} sx={{ mr: 'auto' }}>
+                        <IconButton
+                            onClick={onMenuClick}
+                            sx={{ display: isSmDown ? '' : 'none' }}
+                        >
+                            <MenuRoundedIcon />
+                        </IconButton>
                         <Link
                             component={RLink}
                             to="/"
                             sx={{ fontFamily: 'Anton SC, sans-serif', fontSize: '25px', color: 'black', textDecoration: 'none' }}
                             tabIndex={-1}
                         >JPDM</Link>
-                    </Box>
-                    <Stack component="nav" direction={'row'} flex="1" alignContent={'center'} spacing={2} justifyContent={'center'}>
-                        <Link component={RLink} to="/" sx={linkSx}>Home</Link>
-                        <Divider orientation="vertical" flexItem />
-                        <Link component={RLink} to="/projects" sx={linkSx}>Projects</Link>
-                        <Divider orientation="vertical" flexItem />
-                        <Link component={RLink} to="/homelab" sx={linkSx}>Homelab</Link>
-                        <Divider orientation="vertical" flexItem />
-                        <Link component={RLink} to="/about" sx={linkSx}>About</Link>
-                        {/* <Divider orientation="vertical" flexItem />
-                        <Link component={RLink} to="/services" sx={linkSx}>Hire Me</Link> */}
+                    </Stack>
+                    <Stack
+                        component="nav"
+                        direction={'row'}
+                        flex="1"
+                        spacing={2}
+                        justifyContent={'center'}
+                        sx={{ display: isSmDown ? 'none' : '' }}
+                    >
+                        {menuItems.filter((item) => item.visible).map((item, index, arr) => (
+                            <>
+                                <Link component={RLink} to={item.to} sx={linkSx}>{item.label}</Link>
+                                {index < arr.length - 1 && <Divider orientation="vertical" flexItem />}
+                            </>
+                        ))}
                     </Stack>
                     <UserPanel sx={{ ml: 'auto', width: '55px' }} />
                 </Stack>
