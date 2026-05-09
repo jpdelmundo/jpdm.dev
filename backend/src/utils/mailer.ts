9fd59956a86d8878c2c4ed294b6e599255226250
JP Del Mundo <JP@CZ>
2026-05-09 12:47:34 +0800

    Fixed: Mail config to work on dev and prod

--- backend/src/utils/mailer.ts
+++ backend/src/utils/mailer.ts
@@ -1,6 +1,25 @@
 import nodemailer from 'nodemailer';
 
 export const mail = async ({ from, to, subject, text }: { from: string, to: string, subject: string, text: string }) => {
+    const user = process.env.SMTP_USER;
+    const pass = process.env.SMTP_PASS;
+    const servername = process.env.SMTP_SERVER_NAME;
+    const auth = user && pass ? {
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    } : {};
+    const tls = {
+        tls: {
+            // key: fs.readFileSync(String(process.env.SMTP_CLIENT_KEY)),
+            // cert: fs.readFileSync(String(process.env.SMTP_CLIENT_CERT)),
+            ...(servername && { servername }),
+            rejectUnauthorized: process.env.NODE_ENV === 'production' //false on dev only, true makes sure the "host" param matches the host in the mail server cert
+        }
+    }
+
+    console.log({ auth });
     try {
         console.log(process.env.SMTP_HOST, process.env.SMTP_PORT);
         const transporter = nodemailer.createTransport({
@@ -8,16 +27,8 @@ export const mail = async ({ from, to, subject, text }: { from: string, to: stri
             port: Number(process.env.SMTP_PORT),
             //secure: process.env.SMTP_PORT === '465',
             secure: process.env.SMTP_SECURE === 'true', //only true only if using port 465
-            auth: {
-                user: process.env.SMTP_USER,
-                pass: process.env.SMTP_PASS
-            },
-            tls: {
-                // key: fs.readFileSync(String(process.env.SMTP_CLIENT_KEY)),
-                // cert: fs.readFileSync(String(process.env.SMTP_CLIENT_CERT)),
-                servername: process.env.SMTP_SERVER_NAME, //include server name if using ip address
-                rejectUnauthorized: process.env.NODE_ENV === 'production' //false on dev only, true makes sure the "host" param matches the host in the mail server cert
-            }
+            ...auth,
+            ...tls
         });
 
         const result = await transporter.sendMail({ from, to, subject, text });
